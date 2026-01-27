dd3968c126825d95d28906be97bbe19c6df6e0eb
ro-dina <131938022+ro-dina@users.noreply.github.com>
2026-01-28 01:10:28 +0900

    page

--- src/app/view3d/[id]/page.tsx
+++ src/app/view3d/[id]/page.tsx
@@ -82,8 +82,8 @@ export default function Page({ params }: { params: Promise<RouteParams> }) {
         <iframe
           ref={frameRef}
           id="unityFrame"
-          // 将来のID連携用（いまはランダム遷移で誤魔化す）
-          src={`https://ro-dina.github.io/hotel3d/unity/WebGLBuild/index.html`}
+          // URL から Unity 側で id を取得する想定
+          src={`https://ro-dina.github.io/hotel3d/unity/WebGLBuild/index.html?unityObject=HotelAutoWarpReceiver&id=${encodeURIComponent(id)}`}
           className="w-full h-full"
           allowFullScreen
         />
